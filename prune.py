da8832058920bc87aca347a080e95656e89f098b
ChicoChen <chin2839211@gmail.com>
2024-11-11 21:08:58 +0800

    prune redundent columns

---
+++ prune.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("./data/processed(merged version).csv")
+removedColumns = [
+    "Accelerometer",
+    "Ambient light sensor",
+    "Autofocus_Phase autofocus (Dual Pixel)",
+    "Autofocus_Laser autofocus",
+    "Battery replaceability",
+    "Battery type",
+    "Bundled charger",
+    "Depth sensor (TOF 3D)",
+    "Face recognition sensor",
+    "Fast charging",
+    "Fingerprint",
+    "Full charging time (min)",
+    "Height (mm)",
+    "Hybrid slot",
+    "Infrared port",
+    "Multi SIM mode",
+    "NFC*",
+    "Number of SIM*",
+    "Stabilization"
+]
+
+data = data.drop(columns=removedColumns)
+print(data.shape)
+data.to_csv("./data/pruned.csv")
